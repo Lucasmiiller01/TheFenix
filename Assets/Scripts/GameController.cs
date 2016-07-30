2ce3f3de52efea915a1b06574d652490797a3674
Lucas Miiller <lucasmiiller00007@hotmail.com>
2016-07-30 19:01:34 -0300

    Add function "Decrease all props"

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,17 +7,14 @@ using MyExtensions;
 public class GameController : MonoBehaviour
 {
 
-    /*[SerializeField]
+    [SerializeField]
     private Text clock;
- 
 
     private int seconds;
     private int minutes;
-    private int randomSpawn;
-    //public Vector3 props = new Vector3(1000,1000,1000);
-
     public bool inGame;
-    [SerializeField]
+
+    /*[SerializeField]
     private int tempDay;
     //[SerializeField]
     //private Text day;
@@ -26,19 +23,24 @@ public class GameController : MonoBehaviour
 
     [SerializeField]
     private GameObject paper;
+    public Vector3 props = new Vector3(8, 8, 8);
 
     void Start()
     {
-        /*inGame = false;
+        inGame = true;
         seconds = 0;
         minutes = 0;
-        dayActual = 1;*/
+        InvokeRepeating("DecreaseProps", 10f, 10f);
+        //dayActual = 1;
     }
 
 	void FixedUpdate ()
     {
-        
 
+        if (inGame)
+        {
+            Time_();
+        }
 
     }
     public void OnCreate()
@@ -46,41 +48,23 @@ public class GameController : MonoBehaviour
        Instantiate(paper);
           
     }
-
-    /*public void Time_()
+    public void DecreaseProps()
     {
-        if (inGame)
-        {
-            seconds = 240 + Mathf.FloorToInt(Time.fixedTime);
-            minutes = Mathf.FloorToInt(seconds / 60);
-            seconds = seconds - (60 * minutes);
-            if (seconds< 10 && minutes< 10)
-                clock.text = "0" + minutes + ":0" + seconds;
-            else if (minutes< 10)
-                clock.text = "0" + minutes + ":" + seconds;
-            else if (seconds< 10)
-                clock.text = minutes + ":0" + seconds;
-            else
-                clock.text = minutes + ":" + seconds;
-
-          
-          
-        else
-        {
-                seconds = 240 + Mathf.FloorToInt(Time.fixedTime);
-                minutes = Mathf.FloorToInt(seconds / 60);
-                seconds = seconds - (60 * minutes);
-                if (seconds < 10 && minutes < 10)
-                    clock.text = "0" + minutes + ":0" + seconds;
-                else if (minutes < 10)
-                    clock.text = "0" + minutes + ":" + seconds;
-                else if (seconds < 10)
-                    clock.text = minutes + ":0" + seconds;
-                else
-                    clock.text = minutes + ":" + seconds;
-                //day.text = "Day " + dayActual.ToString();    
-
-            }
-        }
-    }*/
+        props -= new Vector3(1, 1, 1);
     }
+
+    public void Time_()
+    {
+        seconds = Mathf.FloorToInt(Time.fixedTime);
+        minutes = Mathf.FloorToInt(seconds / 60);
+        seconds = seconds - (60 * minutes);
+        if (seconds < 10 && minutes < 10)
+            clock.text = "0" + minutes + ":0" + seconds;
+        else if (minutes < 10)
+            clock.text = "0" + minutes + ":" + seconds;
+        else if (seconds < 10)
+            clock.text = minutes + ":0" + seconds;
+        else
+            clock.text = minutes + ":" + seconds;
+    }
+}
