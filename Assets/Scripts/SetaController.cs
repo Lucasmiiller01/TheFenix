bd0a7a8c2561d2c18450e99c834eb3dd6e168d84
Lucas Miiller <lucasmiiller00007@hotmail.com>
2016-07-31 18:29:02 -0300

    HotFix

--- Assets/Scripts/SetaController.cs
+++ Assets/Scripts/SetaController.cs
@@ -8,41 +8,56 @@ public class SetaController : MonoBehaviour {
     [SerializeField]
     private GameController gameController;
 
-    void Start ()
+    void Start()
+    {
+        InvokeRepeating("Actived", 0.2f, 0.2f);
+    }
+    void Update ()
     {
         switch (myType)
         {
             case "Hospital":
                 if (gameController.props.x >= 6)
                 {
-                    Debug.Log("Good Hospital");
-
+                    this.GetComponent<SpriteRenderer>().flipY = false;
                 }
-                else this.GetComponent<SpriteRenderer>().color = Color.red;
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().color = Color.red;
+                    this.GetComponent<SpriteRenderer>().flipY = true;
+                }
 
                 break;
             case "School":
                 if (gameController.props.y >= 6)
                 {
-                    Debug.Log("Good School");
-
+                   this.GetComponent<SpriteRenderer>().flipY = false;
                 }
-                else this.GetComponent<SpriteRenderer>().color = Color.red;
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().color = Color.red;
+                    this.GetComponent<SpriteRenderer>().flipY = true;
+                }
                 break;
             case "Police":
                 if (gameController.props.z >= 6)
                 {
-                    Debug.Log("Good Police");
+                    this.GetComponent<SpriteRenderer>().flipY = false;
 
                 }
-                else this.GetComponent<SpriteRenderer>().color = Color.red;
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().color = Color.red;
+                    this.GetComponent<SpriteRenderer>().flipY = true;
+                }
                 break;
         }
-
+    
     }
-	
-	
-	void Update () {
-	
-	}
+
+    private void Actived()
+    {
+        this.GetComponent<SpriteRenderer>().enabled = !this.GetComponent<SpriteRenderer>().enabled;
+    }
+
 }
